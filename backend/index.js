e48b34c2913e5b22a679039afced4348ce037d69
Aditya Singh <anshumansingh8825@gmail.com>
2025-12-13 16:14:12 +0530

    Fixed index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use("/user", userRouter);
 app.use("/thread", threadRouter);
 app.use("/community", communityRouter);
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
