796319f59dfd0d27573d777f92c694edde383051
Aditya Singh <anshumansingh8825@gmail.com>
2025-12-13 22:19:45 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(
       "https://threads-sand.vercel.app",
       "https://threads-adityasingh7905s-projects.vercel.app",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
